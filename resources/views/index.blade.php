c11391cb705fa61785dab60d71f43a4e9be84192
Christopher Wray <chris@sol.company>
2022-11-29 19:53:50 -0800

    add two column component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,15 @@
 <x-app-layout>
   <x-section.hero-search />
+  <x-section.two-column-image-right>
+    <x-slot:title>
+      Search Plants
+    </x-slot:title>
+    <x-slot:subtitle>
+      Find and save plants.
+    </x-slot:subtitle>
+    <x-slot:paragraph>
+      Needing a certain hard to find plant? Search for it on our site, and save it for later. See what nurseries grow, and find what plants are most common.
+    </x-slot:paragraph>
+
+  </x-section.two-column-image-right>
 </x-app-layout>
