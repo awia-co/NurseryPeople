066002bf4fe786486a531e63e408dec8934cbb3d
Christopher Wray <chris@sol.company>
2022-12-05 14:30:23 -0800

    update hero search bg color and size

--- resources/views/components/section/hero-search.blade.php
+++ resources/views/components/section/hero-search.blade.php
@@ -1,6 +1,6 @@
-<section class="py-40 bg-white">
+<section class="py-60 bg-white">
   <x-structure.container>
-    <h1 class="text-6xl mb-12 text-center font-extrabold tracking-tight">Search for Plants or Nurseries</h1>
+    <h1 class="text-6xl mb-12 text-center font-extrabold tracking-tight">Search for a Plant</h1>
     <form method="GET" action=" {{ route('search') }}" class="mx-auto flex items-center justify-center">
       @csrf
       <x-form.text-input name="search" type="text" placeholder="Search input" class="px-4 py-4 text-lg" />
